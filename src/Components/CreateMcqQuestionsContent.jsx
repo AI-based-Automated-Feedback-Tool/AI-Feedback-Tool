3805502b460061faf88d1b2c3ccc591fe2900976
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-05-17 18:30:35 +0300

    Grab exam id from url

---
+++ src/Components/CreateMcqQuestionsContent.jsx
@@ -0,0 +1,12 @@
+import React, { use } from 'react'
+
+export default function CreateMcqQuestionsContent() {
+    //grab examid from url
+    const {examId} = useParams();
+    
+  return (
+    <div>
+        CreateMcqQuestionsContent
+    </div>
+  )
+}
