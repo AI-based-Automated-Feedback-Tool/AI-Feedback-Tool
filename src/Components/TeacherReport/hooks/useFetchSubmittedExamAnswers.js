7c9c9722166eb647b9c31b73606c511c99d58f49
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-03 17:29:19 +0300

    move forEach inside useEffect to ensure correct hook usage and prevent multiple useEffect calls

--- src/Components/TeacherReport/hooks/useFetchSubmittedExamAnswers.js
+++ src/Components/TeacherReport/hooks/useFetchSubmittedExamAnswers.js
@@ -5,25 +5,26 @@ const useFetchSubmittedExamAnswers = (submissionId, setGlobalError) => {
     const [submittedAnswers, setSubmittedAnswers] = useState([]);
     const [loadingAnswers, setLoadingAnswers] = useState(false);
 
-    submissionId.forEach(id => {
-        useEffect(() => {
-            if (!id) {
-                setSubmittedAnswers([]);
-                setLoadingAnswers(false);
-                return;
-            }
-            setLoadingAnswers(true);
+    useEffect(() => {
+        if (!submissionId || submissionId.length === 0) {
+            setSubmittedAnswers([]);
+            setLoadingAnswers(false);
+            return;
+        }
+        setLoadingAnswers(true);
 
-        getExamSubmissions(id)
-            .then(data => {
+        submissionId.forEach(id => {
+            getExamSubmissions(id)
+                .then(data => {
                 setSubmittedAnswers(prevAnswers => [...prevAnswers, ...data]);
             })
             .catch(error => {
                 setGlobalError(error.message || "Failed to fetch exam submissions");
             })
             .finally(() => setLoadingAnswers(false));
-        }, [id]);
-    })
-    return { submittedAnswers, loadingAnswers  };
+        });
+    }, [submissionId]);
+
+    return { submittedAnswers, loadingAnswers };
 }
 export default useFetchSubmittedExamAnswers;
