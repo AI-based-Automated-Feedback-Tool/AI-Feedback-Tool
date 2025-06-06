964665da2ef459c88c2102ec8cd6087ad973d161
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-06 22:13:40 +0300

    Create naviagation to code question creation

---
+++ src/Components/CreateQuestions/CreateCodeQuestions/CreateCodeQuestionsContent.jsx
@@ -0,0 +1,10 @@
+import { Container, Row, Col, CardHeader, CardBody, Card, Button, Alert, Form } from 'react-bootstrap';
+
+
+export default function CreateCodeQuestionsContent() {
+  return (
+    <Col className="w-100">
+        <p>Content comes here</p>
+    </Col>
+  )
+}
--- src/Components/CreateQuestions/CreateQuestions.jsx
+++ src/Components/CreateQuestions/CreateQuestions.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import CreateMcqQuestionsContent from './CreateMcqQuestions/CreateMcqQuestionsContent';
+import CreateCodeQuestionsContent from './CreateCodeQuestions/CreateCodeQuestionsContent';
 
 export default function CreateQuestions() {
     
@@ -15,8 +16,8 @@ export default function CreateQuestions() {
   return (
     <>
         { questionType === "mcq" ? (
-            <CreateMcqQuestionsContent examId = {examId} question_count = {question_count} />) /*: (questionType === "code") ? 
-                (<CreateCodeQuestionsContent examId = {examId} question_count = {question_count} />) : (questionType === "essay") ? 
+            <CreateMcqQuestionsContent examId = {examId} question_count = {question_count} />) : (questionType === "code") ? 
+                (<CreateCodeQuestionsContent examId = {examId} question_count = {question_count} />) /*: (questionType === "essay") ? 
                     (<CreateEssayQuestionsContent examId = {examId} question_count = {question_count} />) */: null
         }
     </>  
