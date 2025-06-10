7e76d90f3f6ea491f09eba26281be7b08505bd47
Archana Ojha <t3ojar01@students.oamk.fi>
2025-06-10 15:38:28 +0300

    Added ResultContext Result component and rendered them in App.jsx to display the results of a specific student

--- src/App.jsx
+++ src/App.jsx
@@ -15,67 +15,107 @@ import Profile from "./Components/Profile";
 import CourseExamsPage from "./Components/TeachersComponents/CourseExamsPage";
 import ProfilePage from "./Components/TeachersComponents/ProfilePage";
 import StudentLayout from "./Components/StudentsComponents/StudentLayout";
-import { UserProvider } from "./Context/userContext.jsx";
+import { UserProvider, UserContext } from "./Context/userContext.jsx";
 import TeacherReportContent from "./Components/TeacherReport/TeacherReportContent.jsx";
 import Result from "./Components/StudentsComponents/Result.jsx";
-import AIFeedbackPage from './Components/TeachersComponents/AIFeedbackPage';
+import AIFeedbackPage from "./Components/TeachersComponents/AIFeedbackPage";
 import { CourseProvider } from "./Context/courseContext.jsx";
 import { ExamProvider } from "./Context/examContext.jsx";
 import { TaskProvider } from "./Context/taskContext.jsx";
 import Review from "./Components/StudentsComponents/Review.jsx";
 import { ReviewProvider } from "./Context/ReviewContext.jsx";
-import FeedbackSelector from './Components/TeachersComponents/FeedbackSelector';
-import PromptSelector from './Components/TeachersComponents/PromptSelector';
+import FeedbackSelector from "./Components/TeachersComponents/FeedbackSelector";
+import PromptSelector from "./Components/TeachersComponents/PromptSelector";
 import CreateQuestions from "./Components/CreateQuestions/CreateQuestions.jsx";
+import { ResultProvider } from "./Context/ResultContext.jsx";
+import { useContext } from "react";
+
+function AppContent() {
+  //get user id from userContext
+  const { userId } = useContext(UserContext);
+
+  return (
+      <CourseProvider>
+        <ExamProvider>
+          <TaskProvider>
+            <ResultProvider studentId={userId}>
+              <Router>
+                <Routes>
+                  {/*Login and registration and main page route */}
+                  <Route path="/" element={<MainPage />} />
+                  <Route path="/logIn" element={<LogInPage />} />
+                  <Route path="/register" element={<SignInPage />} />
+                  <Route path="/configure-exam" element={<ConfigureExam />} />
+                  {/*Teachers route */}
+                  <Route path="/teacher" element={<ProtectedRoute />}>
+                    <Route element={<TeacherLayout />}>
+                      <Route index element={<TeacherCourses />} />
+                      <Route path="exams" element={<ConfigureExam />} />
+                      <Route
+                        path="exams/:examId/ai-feedback"
+                        element={<AIFeedbackPage />}
+                      />
+                      <Route
+                        path="ai-feedback"
+                        element={<FeedbackSelector />}
+                      />
+                      <Route
+                        path="exams/:examId/prompt-selector"
+                        element={<PromptSelector />}
+                      />
+                      <Route
+                        path="exams/:examId/questions/:questionType"
+                        element={<CreateQuestions />}
+                      />
+                      <Route
+                        path="courses/:course_id/exams"
+                        element={<CourseExamsPage />}
+                      />
+                      <Route path="profile" element={<ProfilePage />} />
+                      <Route path="students" element={""} />
+                      <Route
+                        path="reports"
+                        element={<TeacherReportContent />}
+                      />
+                    </Route>
+                  </Route>
+                  {/*students route */}
+                  <Route path="/dashboard" element={<ProtectedRoute />}>
+                    <Route path="task/:id" element={<TaskPage />} />
+                  </Route>
+                  <Route
+                    path="/student/courses/:userId"
+                    element={<ProtectedRoute />}
+                  >
+                    <Route element={<StudentLayout />}>
+                      <Route index element={<Courses />} />
+                      <Route path="profile" element={<Profile />} />
+                      <Route path="results" element={<Result />} />
+                      <Route path=":courseId/exams" element={<ExamsPage />} />
+                      <Route
+                        path=":courseId/exams/reviews/:submissionId"
+                        element={
+                          <ReviewProvider>
+                            <Review />
+                          </ReviewProvider>
+                        }
+                      />
+                    </Route>
+                  </Route>
+                </Routes>
+              </Router>
+            </ResultProvider>
+          </TaskProvider>
+        </ExamProvider>
+      </CourseProvider>
+  );
+}
 
 function App() {
   return (
     <UserProvider>
-      <CourseProvider>
-        <ExamProvider>
-        <TaskProvider>
-      <Router>
-        <Routes>
-          {/*Login and registration and main page route */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/logIn" element={<LogInPage />} />
-          <Route path="/register" element={<SignInPage />} />          
-          <Route path="/configure-exam" element={<ConfigureExam />} />
-          {/*Teachers route */}
-          <Route path="/teacher" element={<ProtectedRoute />}>
-            <Route element={<TeacherLayout />}>
-              <Route index element={<TeacherCourses />} />
-                <Route path="exams" element={<ConfigureExam />} />
-                <Route path="exams/:examId/ai-feedback" element={<AIFeedbackPage />} />
-                <Route path="ai-feedback" element={<FeedbackSelector />} />
-                <Route path="exams/:examId/prompt-selector" element={<PromptSelector />} />
-                <Route path="exams/:examId/questions/:questionType" element={<CreateQuestions />} />
-                <Route path="courses/:course_id/exams" element={<CourseExamsPage />} />
-                <Route path="profile" element={<ProfilePage />} />
-                <Route path="students" element={""} />
-                <Route path="reports" element={<TeacherReportContent />} />    
-            </Route>
-          </Route>
-          {/*students route */}
-          <Route path="/dashboard" element={<ProtectedRoute />}>
-            <Route path="task/:id" element={<TaskPage />} />
-          </Route>
-          <Route path="/student/courses/:userId" element={<ProtectedRoute />}>
-            <Route element={<StudentLayout />}>
-              <Route index element={<Courses />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="results" element={<Result />} />
-              <Route path=":courseId/exams" element={<ExamsPage />} />
-              <Route path=":courseId/exams/reviews/:submissionId" element={<ReviewProvider><Review /></ReviewProvider>} />
-            </Route>
-          </Route>
-        </Routes>
-      </Router>
-      </TaskProvider>
-      </ExamProvider>
-      </CourseProvider>
+      <AppContent />
     </UserProvider>
- 
   );
 }
 
