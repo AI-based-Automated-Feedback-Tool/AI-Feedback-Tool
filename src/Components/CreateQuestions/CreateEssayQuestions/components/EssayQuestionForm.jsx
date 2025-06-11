fcba826f84c9ea2d0d2de1afd24b40a2bbbcb2d3
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-11 23:16:43 +0300

    Create basic form for essay question creation

--- src/Components/CreateQuestions/CreateEssayQuestions/components/EssayQuestionForm.jsx
+++ src/Components/CreateQuestions/CreateEssayQuestions/components/EssayQuestionForm.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { Form, Button, Container, Row, Col, Card, Alert } from "react-bootstrap";
+
 
 export default function EssayQuestionForm() {
   return (
-    <div>
-        EssayQuestionForm
-    </div>
+    <Card>
+        <Card.Header className='bg-primary text-white'>
+            <h4>Essay Question</h4>
+        </Card.Header>
+        <Card.Body>
+            <Form>
+                <Form.Group className='mb-3'>
+                    <Form.Label className='fw-bold'>Question *</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={2}
+                        placeholder="Enter your essay question here..."
+                    />
+                </Form.Group>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label className='fw-bold'>Attachments</Form.Label>
+                    <Form.Control type="file" />
+                </Form.Group>
+
+                <Row className='mb-3'>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label className='fw-bold'>Word Limit</Form.Label>
+                            <Form.Control type="number" placeholder="Enter the word limit" />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label className='fw-bold'>Points *</Form.Label>
+                            <Form.Control type="number" placeholder="Enter points" />
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Form.Group className='mb-3'>
+                    <Form.Label className='fw-bold'>Grading Notes *</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                        placeholder="Enter any specific instructions for the essay question evaluation..."
+                    />
+                </Form.Group>
+
+                {/* Submit Button */}
+                <div className="d-flex justify-content-end" >
+                    <Button variant="primary" >
+                        ➕ Save Question
+                    </Button>
+                </div>   
+            </Form>
+        </Card.Body>
+    </Card>
   )
 }
