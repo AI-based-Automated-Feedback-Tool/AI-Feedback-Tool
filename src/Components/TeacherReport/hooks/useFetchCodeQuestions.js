7f01ca78bee1b8581dabb97bce9b40b709b3eb0e
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-19 06:28:44 +0300

    add custom hook to fetch code questions by selected exam

---
+++ src/Components/TeacherReport/hooks/useFetchCodeQuestions.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { getCodeQuestions } from "../service/teacherReportsService";
+
+const useFetchCodeQuestions = (selectedExam, setGlobalError) => {
+    const [codeQuestions, setCodeQuestions] = useState([]);
+    const [loadingCode, setLoadingCode] = useState(false);
+
+    useEffect(() => {
+        if (!selectedExam) {
+            setCodeQuestions([]);
+            setLoadingCode(false);
+            return;
+        }
+        setLoadingCode(true);
+
+        getCodeQuestions(selectedExam)
+            .then(data => {
+                setCodeQuestions(data);
+            })
+            .catch(error => {
+                setGlobalError(error.message || "Failed to fetch code questions");
+            })
+            .finally(() => setLoadingCode(false));
+    }, [selectedExam]);
+    return { codeQuestions, loadingCode };
+}
+export default useFetchCodeQuestions;
