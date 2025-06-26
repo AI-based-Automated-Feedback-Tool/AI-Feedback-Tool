521c593ad052599fa1d0641dad727550d41cc407
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-26 08:07:55 +0300

    Add custom hook to fetch and manage exam details state

---
+++ src/Components/Exams/hooks/useExamDetails.js
@@ -0,0 +1,33 @@
+import {  useState, useEffect } from 'react';
+import { getExamDetails } from '../service/examDetailService';
+
+const useExamDetails = ({examId}) => {
+    const [examDetails, setExamDetails] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchExamDetails = async () => {
+            try {
+                const details = await getExamDetails(examId);
+                setExamDetails(details);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (examId) {
+            fetchExamDetails();
+        }
+    }, [examId]);
+
+    return {
+        examDetails,
+        loading,
+        error
+    };
+};
+
+export default useExamDetails;
