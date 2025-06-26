4d4e0925980c68798bff5f3b67b7a6150f3eb837
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-26 12:47:50 +0300

    Show code question details

--- src/Components/Exams/ExamsContent.jsx
+++ src/Components/Exams/ExamsContent.jsx
@@ -27,28 +27,46 @@ export default function ExamsContent() {
 
           <hr />
           <h5 className="mt-4">📝 Questions ({examDetails?.questions.length})</h5>
-          <ListGroup variant="flush">
-            {examDetails?.questions?.map((q, index) => (
-              <ListGroup.Item key={q.question_id} className="mb-3">
-                <strong>Q{index + 1}:</strong> {q.question_text}
-                <div className="mt-2">
-                  {q.options?.map((option, i) => {
-                    const isCorrect = q.answers.includes(option);
-                    return (
-                      <div key={i} className={`px-2 py-1 rounded mb-1 ${isCorrect ? 'bg-success text-white' : 'bg-light'}`}>
-                        {String.fromCharCode(65 + i)}. {option}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="mt-2">
-                  <small>
-                    Points: <strong>{q.points}</strong> | Correct Answer(s): <strong>{q.no_of_correct_answers}</strong>
-                  </small>
-                </div>
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
+          
+          {examDetails?.type === 'mcq' && (
+            <ListGroup variant="flush">
+              {examDetails?.questions?.map((q, index) => (
+                <ListGroup.Item key={q.question_id} className="mb-3">
+                  <strong>Q{index + 1}:</strong> {q.question_text}
+                  <div className="mt-2">
+                    {q.options.map((option, i) => {
+                      const isCorrect = q.answers.includes(option);
+                      return (
+                        <div key={i} className={`px-2 py-1 rounded mb-1 ${isCorrect ? 'bg-success text-white' : 'bg-light'}`}>
+                          {String.fromCharCode(65 + i)}. {option}
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="mt-2">
+                    <small>
+                      Points: <strong>{q.points}</strong> | Correct Answer(s): <strong>{q.no_of_correct_answers}</strong>
+                    </small>
+                  </div>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+
+          {examDetails?.type === 'code' && (
+            <ListGroup variant="flush">
+              {examDetails?.questions?.map((q, index) => (
+                <ListGroup.Item key={q.question_id} className="mb-3">
+                  <strong>Q{index + 1}:</strong> {q.question_description}
+                  <p className="mb-1 mt-2 "><strong>Function Signature:</strong></p>
+                  <pre className="bg-light p-2 mt-2"><code>{q.function_signature}</code></pre>
+                  <p className="mb-1"><strong>Wrapper Code:</strong></p>
+                  <pre className="bg-secondary text-white p-2 rounded"><code>{q.wrapper_code}</code></pre>
+                  <p><strong>Points:</strong> {q.points}</p>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
         </Card.Body>
       </Card>
     </Col>
