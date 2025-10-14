29a4b12ab0e44132c877e89bba659181c3920f1a
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-10-14 12:04:49 +0300

    Refactor previous code to enter ai powered code question creation form. Create tab nanvigation.

--- src/Components/CreateQuestions/CreateCodeQuestions/components/CodeQuestionForm.jsx
+++ src/Components/CreateQuestions/CreateCodeQuestions/components/CodeQuestionForm.jsx
@@ -1,52 +1,12 @@
 import React from 'react'
-import { Form, Button, Container, Row, Col, Card, Alert } from "react-bootstrap";
-import useFetchLanguages from "../hooks/useFetchLanguages";
+import { Card } from "react-bootstrap";
+import { useState } from 'react';
+import { Nav } from "react-bootstrap";
+import ManualCodeQuestionCreationForm from './ManualCodeQuestionCreationForm';
+import CodeQuestionGenerationForm from './CodeQuestionGenerationForm';
 
 export default function CodeQuestionForm({setError, onAddQuestion, formState, disabled}) {
-
-    const {languages, loading} = useFetchLanguages(setError);
-    const {
-        questionDescription, 
-        setQuestionDescription, 
-        functionSignature, 
-        setFunctionSignature, 
-        wrapperCode, 
-        setWrapperCode, 
-        testCases, 
-        setTestCases, 
-        selectedLanguage, 
-        setSelectedLanguage,
-        points,
-        setPoints,
-        addTestCase,
-        errors,
-        setErrors,
-        validate,
-        resetForm
-    } = formState;
-    
-
-    const handleAddQuestion = () => {
-        // Validate and submit the question
-        const isValid = validate();
-        if (!isValid) {
-            setError({"message": "Please fill in all required fields correctly."});
-            return;
-        }
-
-        const newQuestion = {
-            question: questionDescription,
-            functionSignature: functionSignature,
-            wrapperCode: wrapperCode,
-            testCases: testCases,
-            language: selectedLanguage,
-            points: points
-        }
-        onAddQuestion(newQuestion);
-
-        // Reset form fields
-        resetForm();
-    }
+    const [activeTab, setActiveTab] = useState('manual'); // 'manual' or 'ai'
 
     return (
         <Card>
@@ -54,114 +14,36 @@ export default function CodeQuestionForm({setError, onAddQuestion, formState, di
                 <h4>📝 Create Code Questions</h4>
             </Card.Header>
             <Card.Body>
-                <Form>
-                    <Form.Group className="mb-3">
-                        <Form.Label className="fw-bold">Question Description*</Form.Label>
-                        <Form.Control 
-                            as="textarea" 
-                            rows={2} className="fs-6"
-                            placeholder='Enter your question here...'
-                            value={questionDescription}
-                            onChange={(e) => setQuestionDescription(e.target.value)}
+                <div>
+                <div className="flex border-b">
+                    <Nav variant="tabs" activeKey={activeTab} onSelect={(k) => setActiveTab(k)}>
+                        <Nav.Item>
+                            <Nav.Link eventKey="manual" className={activeTab === 'manual' ? 'bg-primary text-white' : ''}>
+                                Manual Question Creation
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="ai" className={activeTab === 'ai' ? 'bg-primary text-white' : ''}>
+                                AI-Assisted Question Creation
+                            </Nav.Link>
+                        </Nav.Item>
+                        </Nav>
+                </div>
+
+                <div className="mt-4">
+                    {activeTab === 'manual' && (
+                        <ManualCodeQuestionCreationForm
+                            onAddQuestion={onAddQuestion}
+                            setError={setError}
+                            formState={formState}
+                            disabled={disabled}
                         />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label className="fw-bold">Function Signature*</Form.Label>
-                        <Form.Control 
-                            as="textarea" 
-                            rows={2} className="fs-6"
-                            placeholder='Enter the function signature here...'
-                            value={functionSignature}
-                            onChange={(e) => setFunctionSignature(e.target.value)}  
-                        />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label className="fw-bold">Wrapper code*</Form.Label>
-                        <Form.Control 
-                            as="textarea" 
-                            rows={2} className="fs-6"
-                            placeholder='Enter the wrapper code here...'
-                            value={wrapperCode}
-                            onChange={(e) => setWrapperCode(e.target.value)}
-                        />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label className="fw-bold">Test Cases *</Form.Label>
-                        {testCases.map((testCase, index) => (
-                            <Row className='mb-2' key={index}>
-                                <Col>
-                                    <Form.Control 
-                                        placeholder='Input'
-                                        value={testCase.input}
-                                        onChange={(e) => {
-                                            const newTestCase = [...testCases];
-                                            newTestCase[index].input = e.target.value
-                                            setTestCases(newTestCase)
-                                        } }
-                                    />
-                                </Col>
-                                <Col>
-                                    <Form.Control
-                                        placeholder='Output'
-                                        value={testCase.output}
-                                        onChange={(e) => {
-                                            const newTestCase = [...testCases];
-                                            newTestCase[index].output = e.target.value
-                                            setTestCases(newTestCase)
-                                        } }
-                                    />
-                                </Col>
-                                <Col xs="auto">
-                                    <Button variant="outline-secondary" onClick={addTestCase}>+</Button>
-                                </Col>
-                            </Row>
-                        ))}
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label className="fw-bold">Programming Language *</Form.Label>
-                            <Form.Select
-                                value={selectedLanguage?.id || ""}
-                                onChange={(e) => {
-                                    const selectedLang = languages.find(lang => String(lang.id) === e.target.value);
-                                    setSelectedLanguage(selectedLang);
-                                }}
-                            >
-                                {languages.map((language) => (
-                                    <option key={language.id} value={language.id}>
-                                        {language.language_name}
-                                    </option>
-                                ))}
-                            </Form.Select>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label className="fw-bold">Number of points *</Form.Label>
-                            <Form.Control
-                                type="number"
-                                min="1"
-                                placeholder="Enter points for this question"
-                                value={points}
-                                onChange={(e) => setPoints(Number(e.target.value))}
-                                required
-                            />
-                    </Form.Group>
-
-                    {/* Submit Button */}
-                    <div className="d-flex justify-content-end" >
-                        <Button 
-                            variant="primary" 
-                            onClick={handleAddQuestion} 
-                            disabled= {loading || disabled}
-                        >
-                            ➕ Save Question
-                        </Button>
-                    </div>
-
-                </Form>
+                    )}
+                    {activeTab === 'ai' && (
+                        <CodeQuestionGenerationForm/>
+                    )}
+                </div>
+            </div>        
             </Card.Body>
         </Card>
     )
---
+++ src/Components/CreateQuestions/CreateCodeQuestions/components/CodeQuestionGenerationForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CodeQuestionGenerationForm() {
+  return (
+    <div>CodeQuestionGenerationForm</div>
+  )
+}
