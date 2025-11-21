d24cdc3e7b575270d42ce981bae275a3ccf33c8c
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-11-21 11:54:45 +0200

    Update edit mcq questions interface to modern UI

--- src/Components/Exams/components/EditExam.jsx
+++ src/Components/Exams/components/EditExam.jsx
@@ -292,13 +292,6 @@ export default function EditExam({formState, examId, show, handleClose, onSaveSu
             <div className="divider mb-4"></div>
 
             <div className="questions-section mt-5">
-                <div className="d-flex justify-content-between align-items-center mb-4">
-                    <h5 className="mb-0 text-indigo fw-bold">
-                    <i className="fas fa-list-ol me-3"></i>
-                    Questions ({questions?.length || 0})
-                    </h5>
-                </div>
-
                 { examDetails?.type === 'mcq' && <EditMcqQuestionCollection
                     questions={questions}
                     setQuestions={setQuestions}
